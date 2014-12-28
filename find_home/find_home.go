7d030215334bf7ab6bff2ca71741a6d00987ade5
Sebastian Steiger <sebi.steiger@gmail.com>
2014-12-28 13:43:17 +0000

    Testing GAE.

--- find_home/find_home.go
+++ find_home/find_home.go
@@ -1,11 +1,17 @@
-package main
+// For a stand-alone Go program this code would be in package main.
+// The Go App Engine Runtime provides a special main package, so you should put
+// HTTP handler code in a package of your choice.
+package find_home
 
 import (
   "fmt"
-  "time"
+  "net/http"
 )
 
-func main() {
-  fmt.Println("Welcome to the playground!")
-  fmt.Println("The time is", time.Now())
+func init() {
+  http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, "Hello, world!")
 }
